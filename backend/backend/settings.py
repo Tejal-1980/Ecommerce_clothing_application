e6ab34745271f368e744ba7785978b8b6e03dbb4
Tejal <tejaiswal1980@gmail.com>
2026-07-30 11:56:20 +0530

    Update Render settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,9 +29,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
-ALLOWED_HOSTS = ["*"]
-
-# Application definition
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
+CSRF_TRUSTED_ORIGINS = [
+    "https://ecommerce-clothing-application-1.onrender.com",
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
